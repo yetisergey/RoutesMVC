0164429035a2041d282138d5cd4efb831aecd284
Сергей Масленников <yetisergey@gmail.com>
2018-03-06 18:00:39 +0300

    project

---
+++ Routes/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿namespace Routes
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "ProjectsList",
+                url: "Projects",
+                defaults: new { controller = "Projects", action = "ListProjects" }
+            );
+
+            routes.MapRoute(
+                name: "ProjectsActions",
+                url: "Projects/{name}/{action}",
+                defaults: new { controller = "Projects", action = "Index", name = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
---
+++ Routes/Controllers/ProjectsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Routes.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public static List<string> projects = new List<string>() { "odms", "rmpa" };
+        [ActionName("ListProjects")]
+        public string Index()
+        {
+            return string.Join(", ", projects);
+        }
+        public string Index(string name)
+        {
+            if (projects.Contains(name))
+            {
+                return name;
+            }
+            else
+            {
+                return "Project not found";
+            }
+        }
+        public string Settings(string name)
+        {
+            if (projects.Contains(name))
+            {
+                return "Settings of " + name;
+            }
+            else
+            {
+                return "Project not found"; 
+            }
+        }
+    }
+}
